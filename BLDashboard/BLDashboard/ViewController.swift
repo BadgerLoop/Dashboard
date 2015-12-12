d2b34cb3db7004a4bec04c99113a5f68814559b7
croossin <croossin94@gmail.com>
2015-12-12 17:29:35 -0600

    Added dashboard

--- BLDashboard/BLDashboard/ViewController.swift
+++ BLDashboard/BLDashboard/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import Riffle
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RiffleDelegate {
+
+    var app: RiffleDomain?
+    var me: RiffleDomain?
+    var container: RiffleDomain?
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //This is your apps backend - Find it in Backend/main.swift
+        app = RiffleDomain(domain: "xs.demo.badgerloop.bldashboard")
+
+        //Set up your domain
+        me = RiffleDomain(name: "localagent", superdomain: app!)
+        me!.delegate = self
+
+
+        //Joining container with your token
+        //Copy from: Auth() -> Authorized Key Management -> 'localagent' key
+        me!.join("A-ox9WOI.mSxcJ8dQCdyVunYtISCPptVG1IuSsLU7iV2DVxFMhLeaU8EaHzlhWeB3xlZ6FBqHQTRpZmUcXtxVnqH6wzgxoJEk3KfwcEd1KwRe1DpQ3oURbSzt5P-F.F95bEp-nTE.lGiphtFXwRQ0CXx-a14QocjsZWw4-eCZmk_")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Function called when joining backend ran successfuly
+    func onJoin() {
+        print("User joined!")
+        container = RiffleDomain(name: "container", superdomain: app!)
     }
 
-
+    func onLeave() {
+        print("Session left!")
+    }
 }
 
--- BLDashboard/Backend/main.swift
+++ BLDashboard/Backend/main.swift
@@ -7,6 +7,25 @@
 //
 
 import Foundation
+import Riffle
 
-print("Hello, World!")
+print("Starting up the backend...")
 
+//This is your apps backend
+//Change USERNAME to your username that you used to sign up with at my.exis.io
+let app = RiffleDomain(domain: "xs.demo.badgerloop.bldashboard")
+
+class ContainerAgent: RiffleDomain {
+    override func onJoin() {
+        print("Backend fully setup")
+    }
+}
+
+//Your container
+let container = ContainerAgent(name: "container", superdomain: app)
+
+//Joining container with your token
+//Copy from: Auth() -> Authorized Key Management -> 'container' key
+container.join("jRyWJzO6nbZeCvjnYcddp3TsblyACIWg00sRAe6GiZFYVs534rH4dQdeNqDD1n5Eylffo8Uep4wSYdV4dAKGwZ4HQ4cwLNJD8.9QjMopiagDW1b3nJ66isicMFh4TG.Xhx7h8IuDjpCn1D2NsA-4AKM5xDqUqlUr8xfr14cicCw_")
+
+NSRunLoop.currentRunLoop().run()
