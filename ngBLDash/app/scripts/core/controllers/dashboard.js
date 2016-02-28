ea0568e7caab94670a85f7055735241986306bb3
croossin <croossin94@gmail.com>
2016-02-28 10:23:29 -0600

    Working on moving to services

--- ngBLDash/app/scripts/core/controllers/dashboard.js
+++ ngBLDash/app/scripts/core/controllers/dashboard.js
@@ -1,5 +1,5 @@
 angular.module('theme.core.dashboard', ['theme.core.services'])
-  .controller('DashboardController', ['$scope', '$theme', '$timeout', '$window','pinesNotifications', function($scope, $theme, $timeout, $window, pinesNotifications) {
+  .controller('DashboardController', ['$scope', '$theme', '$timeout', '$window','pinesNotifications', 'BatteryService', function($scope, $theme, $timeout, $window, pinesNotifications, BatteryService) {
     'use strict';
     
     //
@@ -22,84 +22,44 @@ angular.module('theme.core.dashboard', ['theme.core.services'])
     // BATTERY VOLTAGE MONITOR
     //
     //
-    $scope.currentBatVoltage;
+    BatteryService.updateRealtimeData();
+    $scope.batt = BatteryService;
+    // $scope.batt.updateRealtimeData();
 
-    var dxta = [],
-      totalPoints = 20;
-    var updateInterval = 1000;
+    // var dxta = [],
+    //   totalPoints = 20;
+    // var updateInterval = 1000;
 
-    function getRandomData() {
-      if (dxta.length > 0) {
-        dxta = dxta.slice(1);
-      }
+    // function getRandomData() {
+    //   if (dxta.length > 0) {
+    //     dxta = dxta.slice(1);
+    //   }
 
-      while (dxta.length < totalPoints) {
-        var prev = dxta.length > 0 ? dxta[dxta.length - 1] : 25,
-            y =  Math.random() * 12;
+    //   while (dxta.length < totalPoints) {
+    //     var prev = dxta.length > 0 ? dxta[dxta.length - 1] : 25,
+    //         y =  Math.random() * 12;
 
-        if (y < 0) {
-            y = 0;
-            // $scope.severeAlert('BATTERY LEVEL ZERO', 'This needs electrical team attention immediately.');
-        } else if (y > 12) {
-            y = 12;
-        }
+    //     if (y < 0) {
+    //         y = 0;
+    //         // $scope.severeAlert('BATTERY LEVEL ZERO', 'This needs electrical team attention immediately.');
+    //     } else if (y > 12) {
+    //         y = 12;
+    //     }
 
-        $scope.currentBatVoltage = Math.round(y);
+    //     $scope.currentBatVoltage = Math.round(y);
 
-        dxta.push(y);
-      }
-      var res = [];
-      for (var i = 0; i < dxta.length; ++i) {
-        res.push([i, dxta[i]]);
-      }
-      return res;
-    }
+    //     dxta.push(y);
+    //   }
+    //   var res = [];
+    //   for (var i = 0; i < dxta.length; ++i) {
+    //     res.push([i, dxta[i]]);
+    //   }
+    //   return res;
+    // }
 
-    // Real Time Data
+    // // Real Time Data
 
-    $scope.realtimeData = [getRandomData()];
-    $scope.realtimeOptions = {
-        series: {
-            bars: {
-                show: true,
-                lineWidth: 0,
-                barWidth: 0.75,
-                fill: 0.4
-            },
-            shadowSize: 0
-        },
-        grid: {
-            labelMargin: 8,
-            hoverable: true,
-            clickable: true,
-            borderWidth: 0,
-            borderColor: '#f5f5f5'
-        },
-        yaxis: {
-            min: 0,
-            max: 15,
-            ticks: [0, 7, 15],
-            tickColor: '#f5f5f5', 
-            font: {color: '#bdbdbd', size: 12}
-        },
-        xaxis: {
-            show: false
-        },
-        colors: ['#00bcd4'],
-        tooltip: true,
-        tooltipOpts: {
-            content: "Voltage: %yV"
-        }
-    };
-
-    var promise;
-    var updateRealtimeData = function() {
-      $scope.realtimeData = [getRandomData()];
-      $timeout.cancel(promise);
-      promise = $timeout(updateRealtimeData, updateInterval);
-    };
-
-    updateRealtimeData();
+    
 
 
     var fans = [[1, 17], [2, 34], [3, 73], [4, 47], [5, 90], [6, 70], [7, 40]];
---
+++ ngBLDash/app/scripts/core/services/BatteryService.js
@@ -0,0 +1,86 @@
+angular
+  .module('theme.core.services')
+  .factory('BatteryService',['$timeout', function($timeout) {
+    'use strict';
+
+    this.currentBatVoltage = 0;
+    var dxta = [],
+      totalPoints = 20;
+    var updateInterval = 1000;
+
+    var getRandomData = function() {
+      if (dxta.length > 0) {
+        dxta = dxta.slice(1);
+      }
+
+      while (dxta.length < totalPoints) {
+        var prev = dxta.length > 0 ? dxta[dxta.length - 1] : 25,
+            y =  Math.random() * 12;
+
+        if (y < 0) {
+            y = 0;
+            // $scope.severeAlert('BATTERY LEVEL ZERO', 'This needs electrical team attention immediately.');
+        } else if (y > 12) {
+            y = 12;
+        }
+
+        this.currentBatVoltage = Math.round(y);
+
+        dxta.push(y);
+      }
+      var res = [];
+      for (var i = 0; i < dxta.length; ++i) {
+        res.push([i, dxta[i]]);
+      }
+      
+      return res;
+
+    }
+
+    this.realtimeData = [getRandomData()];
+    this.realtimeOptions = {
+        series: {
+            bars: {
+                show: true,
+                lineWidth: 0,
+                barWidth: 0.75,
+                fill: 0.4
+            },
+            shadowSize: 0
+        },
+        grid: {
+            labelMargin: 8,
+            hoverable: true,
+            clickable: true,
+            borderWidth: 0,
+            borderColor: '#f5f5f5'
+        },
+        yaxis: {
+            min: 0,
+            max: 15,
+            ticks: [0, 7, 15],
+            tickColor: '#f5f5f5', 
+            font: {color: '#bdbdbd', size: 12}
+        },
+        xaxis: {
+            show: false
+        },
+        colors: ['#00bcd4'],
+        tooltip: true,
+        tooltipOpts: {
+            content: "Voltage: %yV"
+        }
+    };
+
+    var promise;
+    this.updateRealtimeData = function() {
+      realtimeData = [getRandomData()];
+      $timeout.cancel(promise);
+      promise = $timeout(updateRealtimeData, updateInterval);
+    };
+
+    return{
+      updateRealtimeData: function updateRealtimeData(){}
+    };
+
+  }]);
