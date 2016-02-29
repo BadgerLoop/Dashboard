8b937921294d0f3a87ce5df42422fec8dc528932
croossin <croossin94@gmail.com>
2016-02-29 16:17:34 -0600

    Added message list from selected sensors

--- ngBLDash/app/scripts/core/services/SensorService.js
+++ ngBLDash/app/scripts/core/services/SensorService.js
@@ -14,51 +14,58 @@ angular
         {
           sensor: "Shaft Encoder 1",
           data: "12 rpm",
-          id: 1,
+          id: "1",
           type: "Sensor",
-          module: "BPM1"
+          module: "BPM1",
+          location: "Brake Axle"
         },
         {
           sensor: "Limit Switch 1",
           data: "120 F",
-          id: 3,
+          id: "3",
           type: "Sensor",
-          module: "BPM1"
+          module: "BPM1",
+          location: "Boggie - Front left"
         },
         {
           sensor: "Pressure Transducer 1",
           data: "98lb",
-          id: 104,
+          id: "104",
           type: "Sensor",
-          module: "ECM"
+          module: "ECM",
+          location: "Cabin"
         },
         {
           sensor: "Thermistor 11",
           data: "128 F",
-          id: 115,
+          id: "115",
           type: "Sensor",
-          module: "ECM"
+          module: "ECM",
+          location: "BPM2 - solid state relay"
         },
         {
           sensor: "Motor Controller 8",
           data: "24mph",
-          id: 82,
+          id: "82",
           type: "Actuator",
-          module: "MCM"
+          module: "MCM",
+          location: "Motor 8 - Middle Right"
         },
         {
           sensor: "Solid State Relay 3",
           data: "1345",
-          id: 37,
+          id: "37",
           type: "Actuator",
-          module: "BPM2"
+          module: "BPM2",
+          location: "Linear Actuator"
         },
         {
           sensor: "Drum Brake Electromagnet 1",
           data: "2.34",
-          id: 44,
+          id: "44",
           type: "Actuator",
-          module: "BPM2"
+          module: "BPM2",
+          location: "Drum Brake Left Wheel Front"
         }
     ];
 
@@ -67,6 +74,21 @@ angular
       tempMessage.timeStamp = new Date();
       currentMessageList.unshift(tempMessage);
       return currentMessageList;
+    }
+
+    //Check all the selected sensors and add return all the messages from that sensor
+    this.selectedSensorMessageList = function(){
+      var selectedMessages = [];
+
+      for(var sensor in self.selectedSensors){
+        var sensorID = self.selectedSensors[sensor]["SSID"];
+        var temp = self.messageList.filter(function(message){
+          return message.id === sensorID;
+        });
+        selectedMessages.push.apply(selectedMessages, temp);
+      }
+
+      return selectedMessages; 
     }
 
     this.messageList = getMessageList();
