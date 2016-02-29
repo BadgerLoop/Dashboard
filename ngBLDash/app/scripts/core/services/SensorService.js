e4ef80d1563bfa61c1ce468c6080a8da0360ae93
croossin <croossin94@gmail.com>
2016-02-28 18:33:03 -0600

    added dynamic time since function for can message log

--- ngBLDash/app/scripts/core/services/SensorService.js
+++ ngBLDash/app/scripts/core/services/SensorService.js
@@ -40,7 +40,9 @@ angular
     ];
 
     var getMessageList = function() {
-      currentMessageList.push(fakeMessages[Math.floor(Math.random() * fakeMessages.length)]);
+      var tempMessage = fakeMessages[Math.floor(Math.random() * fakeMessages.length)];
+      tempMessage.timeStamp = new Date();
+      currentMessageList.push(tempMessage);
       return currentMessageList;
     }
 
@@ -52,4 +54,33 @@ angular
       $timeout.cancel(promise);
       promise = $timeout(self.updateRealtimeData, updateInterval);
     };
+
+    this.timeSince = function(date){
+
+      var seconds = Math.floor((new Date() - date) / 1000);
+
+      var interval = Math.floor(seconds / 31536000);
+
+      if (interval > 1) {
+          return interval + " years";
+      }
+      interval = Math.floor(seconds / 2592000);
+      if (interval > 1) {
+          return interval + " months";
+      }
+      interval = Math.floor(seconds / 86400);
+      if (interval > 1) {
+          return interval + " days";
+      }
+      interval = Math.floor(seconds / 3600);
+      if (interval > 1) {
+          return interval + " hours";
+      }
+      interval = Math.floor(seconds / 60);
+      if (interval > 1) {
+          return interval + " minutes";
+      }
+      return Math.floor(seconds) + " seconds";
+    };
+
   }]);
