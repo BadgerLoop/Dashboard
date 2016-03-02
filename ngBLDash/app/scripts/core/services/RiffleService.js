3f38321f929077d60e7c17ac2f103662b6f1c4b2
croossin <croossin94@gmail.com>
2016-03-01 22:10:22 -0600

    exis cleanup

--- ngBLDash/app/scripts/core/services/RiffleService.js
+++ ngBLDash/app/scripts/core/services/RiffleService.js
@@ -1,17 +1,14 @@
 angular
     .module('theme.core.services')
-    .service('RiffleService', ['$timeout', '$riffle', '$rootScope', function($timeout, $riffle, $rootScope) {
+    .service('RiffleService', ['$timeout', '$riffle', '$rootScope', 'SensorService', function($timeout, 
+      $riffle, $rootScope, SensorService) {
         'use strict';
 
         var self = this;
 
         //listen live for status updates
         $rootScope.$on('$riffle.open', function() {
-            $riffle.subscribe("temp", update);
+            $riffle.subscribe("temp", SensorService.getMessageList);
         });
-
-        function update(data){
-          console.log(data);
-        }
 
     }]);
--- ngBLDash/app/scripts/core/services/SensorService.js
+++ ngBLDash/app/scripts/core/services/SensorService.js
@@ -1,16 +1,9 @@
 angular
     .module('theme.core.services')
-    .service('SensorService', ['$timeout', '$riffle', '$rootScope', function($timeout, $riffle, $rootScope) {
+    .service('SensorService', ['$timeout', '$riffle', function($timeout, $riffle) {
         'use strict';
 
         var self = this;
-
-
-        //listen live for status updates
-        $rootScope.$on('$riffle.open', function() {
-            $riffle.subscribe("temp", getMessageList);
-        });
-
 
         this.selectedSensors = [];
 
@@ -19,76 +12,11 @@ angular
 
         var modulesMessageCount = { "BPM1": 0, "BPM2": 0, "MCM": 0, "VCM": 0, "ECM": 0 };
 
-        var fakeMessages = [{
-            sensor: "Shaft Encoder 1",
-            data: "12 rpm",
-            id: "1",
-            type: "Sensor",
-            module: "BPM1",
-            location: "Brake Axle"
-        }, {
-            sensor: "Limit Switch 1",
-            data: "120 F",
-            id: "3",
-            type: "Sensor",
-            module: "BPM1",
-            location: "Boggie - Front left"
-        }, {
-            sensor: "Limit Switch 1",
-            data: "120 F",
-            id: "27",
-            type: "Sensor",
-            module: "BPM2",
-            location: "Boggie - Front left"
-        }, {
-            sensor: "Tape Strip Detector",
-            data: "True",
-            id: "100",
-            type: "Sensor",
-            module: "VCM",
-            location: "Boggie - Front left"
-        }, {
-            sensor: "Pressure Transducer 1",
-            data: "98lb",
-            id: "104",
-            type: "Sensor",
-            module: "ECM",
-            location: "Cabin"
-        }, {
-            sensor: "Thermistor 11",
-            data: "128 F",
-            id: "115",
-            type: "Sensor",
-            module: "ECM",
-            location: "BPM2 - solid state relay"
-        }, {
-            sensor: "Motor Controller 8",
-            data: "24mph",
-            id: "82",
-            type: "Actuator",
-            module: "MCM",
-            location: "Motor 8 - Middle Right"
-        }, {
-            sensor: "Solid State Relay 3",
-            data: "1345",
-            id: "37",
-            type: "Actuator",
-            module: "BPM2",
-            location: "Linear Actuator"
-        }, {
-            sensor: "Drum Brake Electromagnet 1",
-            data: "2.34",
-            id: "44",
-            type: "Actuator",
-            module: "BPM2",
-            location: "Drum Brake Left Wheel Front"
-        }];
-
         /**
          * Create random message, increment module count, timestamp, add to list
          * @return {[Messages]} List of all messages 
          */
-        var getMessageList = function(message) {
+        this.getMessageList = function(message) {
             var tempMessage = message;
             modulesMessageCount[tempMessage.module] ?
                 modulesMessageCount[tempMessage.module].value += 1 :
